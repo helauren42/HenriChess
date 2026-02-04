48df3af27fdbf5217e106d5fe0fd4bd799bd0042
helauren42 <helauren42@gmail.com>
2026-02-04 02:37:57 +0100

    trim down sql db tables removed onlinegames and hotseatgames split for a simplified games table

--- docker/postgres/init.sql
+++ docker/postgres/init.sql
@@ -16,83 +16,24 @@ CREATE TABLE IF NOT EXISTS sessions (
 
 CREATE INDEX idx_sessions_device_token ON sessions(device_token);
 
-CREATE TABLE IF NOT EXISTS agames (
+CREATE TABLE IF NOT EXISTS games (
   id SERIAL PRIMARY KEY,
   creation TIMESTAMP DEFAULT NOW(),
-  winner TEXT CHECK (winner IN ('b', 'w', 'd'))
-);
-
-CREATE TABLE IF NOT EXISTS hotseatgames (
-  id SERIAL PRIMARY KEY,
-  user_id INTEGER REFERENCES users(id) ON DELETE CASCADE NOT NULL
-) INHERITS(agames);
-
-CREATE TABLE IF NOT EXISTS onlinegames (
-  id SERIAL PRIMARY KEY,
+  game_mode TEXT CHECK (game_mode IN ('hotseat', 'online')),
   white_id INTEGER REFERENCES users(id) NOT NULL,
   black_id INTEGER REFERENCES users(id) NOT NULL,
   white_username TEXT NOT NULL,
   black_username TEXT NOT NULL,
-  CHECK (white_id <> black_id)
-) INHERITS(agames);
+  fens TEXT[] NOT NULL,
+  game_moves TEXT[] NOT NULL,
+  winner INTEGER REFERENCES users(id)
+);
+
+CREATE INDEX idx_games_winner on games(winner);
 
 CREATE TABLE IF NOT EXISTS onlinegamesmessages (
   id SERIAL PRIMARY KEY,
-  game_id INT REFERENCES onlinegames(id),
+  game_id INT REFERENCES games(id),
   username TEXT REFERENCES users(username),
   message TEXT NOT NULL
-)
-
-CREATE INDEX idx_hotseat_games_winner on hotseatgames(winner);
-CREATE INDEX idx_online_games_winner on onlinegames(winner);
-
-CREATE TABLE IF NOT EXISTS agamepositions (
-  id SERIAL PRIMARY KEY,
-  position_number INTEGER NOT NULL DEFAULT 0,
-  fen TEXT NOT NULL DEFAULT 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1'
-  -- capturedWhites 
 );
-
-CREATE TABLE IF NOT EXISTS hotseatgamepositions (
-  id SERIAL PRIMARY KEY,
-  game_id INTEGER NOT NULL REFERENCES hotseatgames(id) ON DELETE CASCADE,
-  UNIQUE (position_number, game_id)
-) INHERITS(agamepositions);
-
-CREATE TABLE IF NOT EXISTS onlinegamepositions (
-  id SERIAL PRIMARY KEY,
-  game_id INTEGER NOT NULL REFERENCES onlinegames(id) ON DELETE CASCADE,
-  UNIQUE (position_number, game_id)
-) INHERITS(agamepositions);
-
-CREATE INDEX idx_hotseat_game_positions_game_id on hotseatgamepositions(game_id);
-CREATE INDEX idx_hotseat_game_positions_game_id_position_number on hotseatgamepositions(game_id, position_number);
-
-CREATE INDEX idx_online_game_positions_game_id on onlinegamepositions(game_id);
-CREATE INDEX idx_online_game_positions_game_id_position_number on onlinegamepositions(game_id, position_number);
-
-CREATE TABLE IF NOT EXISTS agamemoves (
-    id SERIAL PRIMARY KEY,
-    move_number INTEGER NOT NULL CHECK(move_number >= 0),
-    game_id INTEGER NOT NULL REFERENCES agames(id) ON DELETE CASCADE,
-    uci TEXT NOT NULL,
-    san TEXT NOT NULL, -- Standard Algebraic Notation
- 
-    UNIQUE (game_id, move_number)
-);
-
-CREATE TABLE IF NOT EXISTS hotseatgamemoves (
-    id SERIAL PRIMARY KEY,
-    game_id INTEGER NOT NULL REFERENCES hotseatgames(id) ON DELETE CASCADE
-) INHERITS(agamemoves);
-
-CREATE TABLE IF NOT EXISTS onlinegamemoves (
-    id SERIAL PRIMARY KEY,
-    game_id INTEGER NOT NULL REFERENCES onlinegames(id) ON DELETE CASCADE
-) INHERITS(agamemoves);
-
-CREATE INDEX idx_hotseat_game_moves_game_id on hotseatgamemoves(game_id);
-CREATE INDEX idx_hotseat_game_moves_game_id_move_number ON hotseatgamemoves(game_id, move_number); -- composite index
-
-CREATE INDEX idx_online_game_moves_game_id on onlinegamemoves(game_id);
-CREATE INDEX idx_online_game_moves_game_id_move_number ON onlinegamemoves(game_id, move_number); -- composite index
