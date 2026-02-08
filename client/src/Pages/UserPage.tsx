fff06dd7912af5b38828d4534550855c2797a0b7
helauren42 <helauren42@gmail.com>
2026-02-08 19:37:01 +0100

    added visibility and changing visibility button on user page although private featurte not yet implemented

--- client/src/Pages/UserPage.tsx
+++ client/src/Pages/UserPage.tsx
@@ -2,7 +2,7 @@ import { useContext, useEffect, useState } from "react"
 import { useLocation } from "react-router-dom"
 import { readFetch, writeFetch, type MyResp } from "../utils/requests"
 import { AuthCompContext } from "../Contexts/AuthComp"
-import { ToastCustomError } from "../utils/toastify"
+import { ToastCustomError, ToastFeatureNotImplemented } from "../utils/toastify"
 
 import "./UserPage.css"
 import { HotseatHistory, type HotseatHistoryFace } from "../componants/HotseatHistory"
@@ -12,6 +12,7 @@ interface UserData {
   username: ""
   email: ""
   creation: ""
+  visibility: "Public" | "Private"
 }
 
 const UserDataDisplay = ({ userData }: { userData: UserData }) => {
@@ -26,18 +27,24 @@ const UserDataDisplay = ({ userData }: { userData: UserData }) => {
   }
   console.log("user data: ", userData)
   return (
-    <div id="user-data-display" className="flex ">
-      <div id="left-side" className="flex flex-col gap-3">
-        <svg xmlns="http://www.w3.org/2000/svg" className="fill-(--button-color)" width="100px" height="100px" viewBox="0 0 24 24"><path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 3c1.66 0 3 1.34 3 3s-1.34 3-3 3-3-1.34-3-3 1.34-3 3-3zm0 14.2c-2.5 0-4.71-1.28-6-3.22.03-1.99 4-3.08 6-3.08 1.99 0 5.97 1.09 6 3.08-1.29 1.94-3.5 3.22-6 3.22z" /></svg>
+    <div id="user-data-display" className="flex flex-col gap-5">
+      <div className="row-block">
+        <div id="profile-pic" className="flex flex-col gap-3">
+          <svg xmlns="http://www.w3.org/2000/svg" className="fill-(--button-color)" width="100px" height="100px" viewBox="0 0 24 24"><path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 3c1.66 0 3 1.34 3 3s-1.34 3-3 3-3-1.34-3-3 1.34-3 3-3zm0 14.2c-2.5 0-4.71-1.28-6-3.22.03-1.99 4-3.08 6-3.08 1.99 0 5.97 1.09 6 3.08-1.29 1.94-3.5 3.22-6 3.22z" /></svg>
+        </div>
+        <div className="flex flex-col pt-3">
+          <h3>{userData.username}</h3>
+          <p>Joined {userData.creation}</p>
+          <p>Visibility: {userData.visibility}</p>
+        </div>
+      </div>
+      <div className="flex gap-3">
         <button onClick={() => {
           localStorage.removeItem("username")
           localStorage.removeItem("email")
           logout()
         }}>Logout</button>
-      </div>
-      <div className="flex flex-col pt-3">
-        <h3>{userData.username}</h3>
-        <p>Joined {userData.creation}</p>
+        <button onClick={() => ToastFeatureNotImplemented()}>Go {userData.visibility == "Private" ? "Public" : "Private"}</button>
       </div>
     </div>
   )
@@ -48,7 +55,8 @@ export const UserPage = () => {
   const [userData, setUserData] = useState<UserData>({
     username: "",
     email: "",
-    creation: ""
+    creation: "",
+    visibility: "Public"
   })
   const [hotseatHistory, setHotseatHistory] = useState<HotseatHistoryFace[]>([])
   const [onlineHistory, setOnlineHistory] = useState<OnlineHistoryFace[]>([])
@@ -58,6 +66,7 @@ export const UserPage = () => {
   const fetchUser = async () => {
     const resp: MyResp = await readFetch(`/user/${username}`)
     if (resp.status == 200 && resp.data) {
+      resp.data["visibility"] = "Public"
       setUserData(resp.data as UserData)
     }
     if (resp.status == 401) {
